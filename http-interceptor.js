0f044597fa9bc43d81097d9564507cdaff4ba10d
未来_加油 <741804631@qq.com>
2019-05-10 17:15:42 +0800

    添加axios请求拦截器

---
+++ http-interceptor.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import { message } from 'ant-design-vue'
+
+axios.interceptors.response.use(
+    response => {
+        if (response.status === 200) {
+            console.log( response );
+            if ( !response.data.data ) {
+                message.error( response.data.errorInfo )
+            } 
+            // console.log('请求成功');
+        }
+        return response
+    }, err => {
+        if( err.response.status === 401 ) {
+            message.error('请求错误'); 
+        }
+        if( err.response.status === 500 ) {
+            message.error('后台出错');            
+        }
+    }
+)
+
+function clearHanlder(){
+    // store.commit('setToken', '');
+    // localStorage.removeItem('token')
+/*     router.push({
+        path: '/login',
+        query: {redirect: router.currentRoute.path}
+        // router.currentRoute.path 回跳到当前页面 
+    })  */    
+}
